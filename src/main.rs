a9f25f18e27c47226232f3851c25dfa48dcda3e1
DjX <karwowski.d@o2.pl>
2025-11-14 21:20:23 +0100

    Add step 3 solution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-#[allow(unused_imports)]
-use std::io::{BufReader, prelude::*};
+use std::io::{prelude::*};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -11,7 +9,6 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("accepted new connection");
                 handle_connection(stream);
             }
             Err(e) => {
@@ -22,16 +19,43 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    println!("accepted new connection");
 
-    let result = stream.write_all(response.as_bytes());
+    let mut request = String::new();
 
-    match result {
-        Ok(_) => {
-            println!("response successfully written to TCP stream");
-        }
-        Err(_) => {
-            println!("failed to write response to TCP stream")
+    if let Ok(_) = stream.read_to_string(&mut request) {
+        println!("{request}");
+
+        let response = match parse_request(&request) {
+            HttpResponse::Ok => "HTTP/1.1 200 OK\r\n\r\n",
+            HttpResponse::BadRequest => "HTTP/1.1 404 Not Found\r\n\r\n",
+        };
+
+        let result = stream.write_all(response.as_bytes());
+
+        match result {
+            Ok(_) => {
+                println!("response successfully written to TCP stream");
+            }
+            Err(_) => {
+                println!("failed to write response to TCP stream")
+            }
         }
     }
+}
+
+fn parse_request(request: &str) -> HttpResponse {
+    let split: Vec<&str> = request.split_whitespace().collect();
+
+    let request_path = split[1];
+
+    match request_path {
+        "/" => HttpResponse::Ok,
+        _ => HttpResponse::BadRequest,
+    }
+}
+
+enum HttpResponse {
+    Ok,
+    BadRequest,
 }
