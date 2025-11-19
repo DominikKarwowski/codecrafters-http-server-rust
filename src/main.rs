3a9a48e4215d82634bb85c50709cd87d18bbad20
DjX <karwowski.d@o2.pl>
2025-11-19 22:20:45 +0100

    Add step 6 solution

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@ mod model;
 
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
+use codecrafters_http_server::ThreadPool;
 use crate::model::HttpRequest;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_connection(stream);
+                pool.execute(|| {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
